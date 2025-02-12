a78f77ac7a30d056f1802691c8bc5994ff800dfa
Gaurav Kumar Singh <gsingh332211@gmail.com>
2025-02-13 01:02:59 +0530

    package.json

--- docs app/src/components/Background.jsx
+++ docs app/src/components/Background.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import packageJson from '../../package.json';
+import packageJson from "../../package.json";
 
 function Background() {
   return (
@@ -11,7 +11,11 @@ function Background() {
         <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-medium text-[#292929]">
           Docs.
         </h1>
-        <footer className="text-white">Version: {packageJson.version}</footer>;
+        <footer className="absolute bottom-0 left-0 w-full bg-gray-900 text-white text-center py-2 shadow-md">
+          <span className="text-sm opacity-80">
+            Version: {packageJson.version}
+          </span>
+        </footer>
       </div>
     </>
   );
